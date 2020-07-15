6a2adc64a7a3d81e1722771c90a81c58f6b34faa
callumpoole <callumpoole06@hotmail.co.uk>
2020-07-15 17:46:34 +0100

    removed main() from client

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -1,9 +1,17 @@
 #include "Poole.h"
-#include <memory>
 
-std::unique_ptr<Poole::Engine> engine;
-
-int main()
+class Sandbox : public Poole::Engine
 {
-	engine = std::make_unique<Poole::Engine>("Poole Engine Sandbox", glm::uvec2(640, 480));
+public:
+	Sandbox() : Engine()
+	{
+	}
+	~Sandbox()
+	{
+	}
+};
+
+Poole::Engine* Poole::CreateApplication()
+{
+	return new Sandbox();
 }
