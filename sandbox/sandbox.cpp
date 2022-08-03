4be1e73bab4192ba1a8be7e75bb70ce9f2103a03
Callum Poole <callumpoole06@hotmail.co.uk>
2022-08-03 22:19:27 +0100

    Relative paths to avoid having these random images on Desktop.

--- sandbox/sandbox.cpp
+++ sandbox/sandbox.cpp
@@ -1,10 +1,14 @@
 #include "sandbox.h"
 
 #include <iostream>
+#include <filesystem>
 #include "poole/rendering/renderer.h"
 #include "poole/rendering/renderer2D.h"
 #include "poole/rendering/rendering_primitives.h"
 #include "poole/input/input.h"
+
+#include "poole/rendering/image/image.h"
+#include "poole/rendering/image/sub_image.h"
 
 static constexpr bool TempRenderSeperately = true;
 
@@ -178,13 +182,33 @@ void TempMoveTriangles()
     Mesh->m_rotation = P;
 }
 
-Poole::Rendering::TextureHandle textureHandle;
+Poole::Rendering::Image textureHandle;
+Poole::Rendering::Image textureHandle2;
+Poole::Rendering::Image textureHandle3;
+std::shared_ptr<Poole::Rendering::Image> spriteSheet;
+std::shared_ptr<Poole::Rendering::SubImage> sub1;
+std::shared_ptr<Poole::Rendering::SubImage> sub2;
+std::shared_ptr<Poole::Rendering::SubImage> sub3;
 
 void Sandbox::BeginApp()
 {
+#define IMAGE_PATH "../../sandbox/images/"
+
     //I know abs paths are not ideal for github, this is temp! And it's in a sandbox project anyways!
-    //textureHandle = Poole::Rendering::Renderer2D::LoadTexture("D:/Callum/Desktop/testTexture.png"); //RGB only!
-    textureHandle = Poole::Rendering::Renderer2D::LoadTexture("D:/Callum/Desktop/testTextureRGBA.png");
+    textureHandle = Poole::Rendering::Image(IMAGE_PATH "testTexture.png"); //RGB only!
+    //textureHandle2 = Poole::Rendering::Image("D:/Callum/Desktop/testTextureRGBA.png");
+    textureHandle2 = Poole::Rendering::Image(IMAGE_PATH "ColorSquare16.png");
+
+    textureHandle3 = Poole::Rendering::Image(IMAGE_PATH "Gary_Redford_95_MJN.jpg");
+    
+    spriteSheet.reset(new Poole::Rendering::Image(IMAGE_PATH "spriteSheet.png"));
+ 
+#undef IMAGE_PATH
+
+    sub1.reset(Poole::Rendering::SubImage::Create(spriteSheet, {3,5}, {16,16}));
+    sub2.reset(Poole::Rendering::SubImage::Create(spriteSheet, {3,4}, {16,16}));
+    sub3.reset(Poole::Rendering::SubImage::Create(spriteSheet, {2,4}, {16,16}));
+    
 
     //TestMeshNoIndiciesSolidColor();
     //TestMeshSolidColor();
@@ -205,8 +229,23 @@ void Sandbox::UpdateApp(float /*deltaTime*/)
    Renderer2D::DrawCircle({ -1.0, 0.f }, { 0.5f, 1.f }, Colors::Red<fcolor4>, 0.f, fvec2(0.f), mouseNorm.x, mouseNorm.y);
    Renderer2D::DrawTexturedQuad({ 0.0, 0.f }, { 0.5f, 0.5f }, textureHandle);
    Renderer2D::DrawQuad({ 0.4, 0.8f }, { 0.25f, 0.2f }, Colors::Yellow<fcolor4>, 0.f, fvec2(0.f));
-   Renderer2D::DrawTexturedQuad({ 0.7f, 0.7f }, { 0.25f, 0.25f }, textureHandle);
+   Renderer2D::DrawTexturedQuad({ 0.9f, 0.7f }, { 0.25f, 0.25f }, textureHandle2);
    Renderer2D::DrawCircle({ 0, 0.8f }, { 0.05f, 0.2f }, Colors::Blue<fcolor4>, 0.f, fvec2(0.f), mouseNorm.x, mouseNorm.y);
+
+
+   Renderer2D::DrawSubTexturedQuad({ -0.75f, 0.75f }, { 0.125f, 0.125f }, *sub1);
+   Renderer2D::DrawSubTexturedQuad({ -0.75f, 0.50f }, { 0.125f, 0.125f }, *sub2);
+
+   Renderer2D::DrawQuad({ 0.0, -0.7f }, { 0.1f, 0.1f }, Colors::Cyan<fcolor4>, 0.f, fvec2(0.f));
+
+   Renderer2D::DrawTexturedQuad({ -0.9f, -0.7f }, { 0.25f, 0.25f }, textureHandle3);
+
+   Renderer2D::DrawSubTexturedQuad({ -1.00f, 0.50f }, { 0.125f, 0.125f }, *sub3);
+
+   Renderer2D::DrawQuad({ 0.5f, -0.7f }, { 0.2f, 0.1f }, Colors::Purple<fcolor4>, 0.f, fvec2(0.f));
+
+   Renderer2D::DrawTexturedQuad({ 0.8f, -0.7f }, { 0.2f, 0.2f }, *spriteSheet);
+
 }
 
 void Sandbox::EndApp()
