5addb69c0aef7b2db4684bd2b9b926630582b965
Callum Poole <callumpoole06@hotmail.co.uk>
2022-08-14 16:57:35 +0100

    Removed Rendering Primitives (First Messy Rendering Structure)

--- sandbox/sandbox.cpp
+++ sandbox/sandbox.cpp
@@ -4,183 +4,10 @@
 #include <filesystem>
 #include "poole/rendering/renderer.h"
 #include "poole/rendering/renderer2D.h"
-#include "poole/rendering/rendering_primitives.h"
 #include "poole/input/input.h"
 
 #include "poole/rendering/image/image.h"
 #include "poole/rendering/image/sub_image.h"
-
-static constexpr bool TempRenderSeperately = true;
-
-void TestMeshNoIndiciesSolidColor()
-{
-    using namespace Poole::Rendering;
-
-    if constexpr (TempRenderSeperately)
-    {
-        StaticMeshNoIndiciesSolidColor4_2DTransform meshNoInd1;
-        meshNoInd1.m_verts = std::vector<fvec3>{
-            {-0.5f,  -0.5f,  0.0f}, { 0.5f,  -0.5f,  0.0f}, { 0.0f,   0.5f,  0.0f},
-        };
-        meshNoInd1.m_color = Colors::Green<fcolor4>;
-        Renderer::Submit(std::move(meshNoInd1));
-
-
-        StaticMeshNoIndiciesSolidColor4_2DTransform meshNoInd2;
-        meshNoInd2.m_verts = std::vector<fvec3>{
-            {-0.75f, -0.75f, 0.0f}, {-0.25f, -0.75f, 0.0f}, {-0.5f,  -0.5f,  0.0f},
-        };
-        meshNoInd2.m_color = Colors::Pink<fcolor4>;
-        Renderer::Submit(std::move(meshNoInd2));
-
-
-        StaticMeshNoIndiciesSolidColor4_2DTransform meshNoInd3;
-        meshNoInd3.m_verts = std::vector<fvec3>{
-            {0.75f, 0.75f, 0.0f}, {0.25f, 0.75f, 0.0f}, {0.5f,  0.5f,  0.0f},
-        };
-        meshNoInd3.m_color = Colors::Cyan<fcolor4>;
-        Renderer::Submit(std::move(meshNoInd3));
-    }
-    else
-    {
-        StaticMeshNoIndiciesSolidColor4_2DTransform meshNoInd;
-        meshNoInd.m_verts = std::vector<fvec3>{
-            {-0.5f,  -0.5f,  0.0f}, { 0.5f,  -0.5f,  0.0f}, { 0.0f,  0.5f, 0.0f},
-            {-0.75f, -0.75f, 0.0f}, {-0.25f, -0.75f, 0.0f}, {-0.5f, -0.5f, 0.0f},
-            { 0.75f,  0.75f, 0.0f}, { 0.25f,  0.75f, 0.0f}, { 0.5f,  0.5f, 0.0f},
-        };
-        meshNoInd.m_color = Colors::Green<fcolor4>;
-        Renderer::Submit(std::move(meshNoInd));
-    }
-}
-void TestMeshSolidColor()
-{
-    using namespace Poole::Rendering;
-
-    if constexpr (TempRenderSeperately)
-    {
-        StaticMeshSolidColor4_2DTransform mesh1;
-        mesh1.m_verts = std::vector<fvec3>{
-            {0.9f,   0.3f, 0.0f}, // top right
-            {0.9f,  -0.3f, 0.0f}, // bottom right
-            {0.6f,  -0.3f, 0.0f}, // bottom left
-            {0.6f,   0.3f, 0.0f}, // top left
-        };
-        mesh1.m_indices = {
-            0, 1, 3, /*first triangle*/ 1, 2, 3, /*second triangle*/
-        };
-        mesh1.m_color = Colors::Red<fcolor4>;
-        Renderer::Submit(std::move(mesh1));
-
-
-        StaticMeshSolidColor4_2DTransform mesh2;
-        mesh2.m_verts = std::vector<fvec3>{
-            { 0.9f,  0.9f, 0.0f}, // top right
-            { 0.9f,  0.7f, 0.0f}, // bottom right
-            { 0.7f,  0.7f, 0.0f}, // bottom left
-            { 0.7f,  0.9f, 0.0f}, // top left
-        };
-        mesh2.m_indices = {
-            0, 1, 3, /*first triangle*/ 1, 2, 3, /*second triangle*/
-        };
-        mesh2.m_color = Colors::Orange<fcolor4>;
-        Renderer::Submit(std::move(mesh2));
-    }
-    else
-    {
-        StaticMeshSolidColor4_2DTransform mesh;
-        mesh.m_verts = std::vector<fvec3>{
-            {0.9f,   0.3f, 0.0f}, // top right
-            {0.9f,  -0.3f, 0.0f}, // bottom right
-            {0.6f,  -0.3f, 0.0f}, // bottom left
-            {0.6f,   0.3f, 0.0f}, // top left
-        
-            { 0.9f,  0.9f, 0.0f}, // top right
-            { 0.9f,  0.7f, 0.0f}, // bottom right
-            { 0.7f,  0.7f, 0.0f}, // bottom left
-            { 0.7f,  0.9f, 0.0f}, // top left
-        };
-        mesh.m_indices = {
-              0,   1,   3, /*first triangle*/   1,   2,   3, /*second triangle*/
-            4+0, 4+1, 4+3, /*third triangle*/ 4+1, 4+2, 4+3, /*fourth triangle*/
-        };
-        mesh.m_color = Colors::Red<fcolor4>;
-        Renderer::Submit(std::move(mesh));
-    }
-}
-void TestMeshVertexColor()
-{
-    using namespace Poole::Rendering;
-
-    if constexpr (TempRenderSeperately)
-    {
-        StaticMeshVertexColor4_2DTransform meshWithColor1;
-        meshWithColor1.m_verts = std::vector<Vertex3Color4>{
-            {fvec3{ 0.2f,   0.2f, 0.0f}, /*top right*/      Colors::Yellow<fcolor4>},
-            {fvec3{ 0.2f,  -0.2f, 0.0f}, /*bottom right*/   Colors::Green<fcolor4>},
-            {fvec3{-0.2f,  -0.2f, 0.0f}, /*bottom left*/    Colors::White<fcolor4>},
-            {fvec3{-0.2f,   0.2f, 0.0f}, /*top left*/       Colors::Purple<fcolor4>},
-        };
-        meshWithColor1.m_indices = {
-            0, 1, 3, /*first triangle*/ 1, 2, 3, /*second triangle*/
-        };
-        meshWithColor1.m_position = { -0.7f, 0.2f };
-        Renderer::Submit(std::move(meshWithColor1));
-
-
-        StaticMeshVertexColor4_2DTransform meshWithColor2;
-        meshWithColor2.m_verts = std::vector<Vertex3Color4>{
-            {fvec3{-0.7f,  0.9f, 0.0f}, /*top right*/      Colors::Orange<fcolor4>},
-            {fvec3{-0.7f,  0.7f, 0.0f}, /*bottom right*/   Colors::Cyan<fcolor4>},
-            {fvec3{-0.9f,  0.7f, 0.0f}, /*bottom left*/    Colors::Grey<fcolor4>},
-            {fvec3{-0.9f,  0.9f, 0.0f}, /*top left*/       Colors::Pink<fcolor4>},
-        };
-        meshWithColor2.m_indices = {
-            0, 1, 3, /*first triangle*/ 1, 2, 3, /*second triangle*/
-        };
-        Renderer::Submit(std::move(meshWithColor2));
-    }
-    else
-    {
-        StaticMeshVertexColor4_2DTransform meshWithColor;
-        meshWithColor.m_verts = std::vector<Vertex3Color4>{
-            {fvec3{ 0.2f,   0.2f, 0.0f}, /*top right*/      Colors::Yellow<fcolor4>},
-            {fvec3{ 0.2f,  -0.2f, 0.0f}, /*bottom right*/   Colors::Green<fcolor4>},
-            {fvec3{-0.2f,  -0.2f, 0.0f}, /*bottom left*/    Colors::White<fcolor4>},
-            {fvec3{-0.2f,   0.2f, 0.0f}, /*top left*/       Colors::Purple<fcolor4>},
-        
-            {fvec3{-0.7f,  0.9f, 0.0f}, /*top right*/      Colors::Orange<fcolor4>},
-            {fvec3{-0.7f,  0.7f, 0.0f}, /*bottom right*/   Colors::Cyan<fcolor4>},
-            {fvec3{-0.9f,  0.7f, 0.0f}, /*bottom left*/    Colors::Grey<fcolor4>}, 
-            {fvec3{-0.9f,  0.9f, 0.0f}, /*top left*/       Colors::Pink<fcolor4>},
-        };
-        meshWithColor.m_indices = {
-              0,   1,   3, /*first triangle*/   1,   2,   3, /*second triangle*/
-            4+0, 4+1, 4+3, /*third triangle*/ 4+1, 4+2, 4+3, /*fourth triangle*/
-        };
-        Renderer::Submit(std::move(meshWithColor));
-    }
-}
-
-void TempMoveTriangles()
-{
-    using namespace Poole::Rendering;
-
-    StaticMeshNoIndiciesSolidColor4_2DTransform* Mesh =
-        Renderer::GetMesh<StaticMeshNoIndiciesSolidColor4_2DTransform>(0);
-
-    static float P = 0.f;
-    P += 0.1f;
-
-    using namespace Poole;
-    const auto cursor = Input::GetMousePositionFloat(true, ECursorClamping::None, ECursorNormalization::CameraNegativeOneToOneAspect);
-
-    LOG("Cursor Position at ({},{})", cursor.x, cursor.y);
-    
-    Mesh->m_position = { cursor.x, cursor.y };
-    Mesh->m_scale = fvec2(0.05f);
-    Mesh->m_rotation = P;
-}
 
 Poole::Rendering::Image textureHandle;
 Poole::Rendering::Image textureHandle2;
@@ -208,17 +35,10 @@ void Sandbox::BeginApp()
     sub1.reset(Poole::Rendering::SubImage::Create(spriteSheet, { 8,8}, {16,16}));
     sub2.reset(Poole::Rendering::SubImage::Create(spriteSheet, { 9,8}, {16,16}));
     sub3.reset(Poole::Rendering::SubImage::Create(spriteSheet, {10,8}, {16,16}));
-    
-
-    //TestMeshNoIndiciesSolidColor();
-    //TestMeshSolidColor();
-    //TestMeshVertexColor();
 }
 
 void Sandbox::UpdateApp(float /*deltaTime*/)
 {
-    //TempMoveTriangles();
-
     using namespace Poole::Rendering;
     using namespace Poole;
 
